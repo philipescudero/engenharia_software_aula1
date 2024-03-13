3c4bac6ba68af393495728217b9a105c1007a181
Philip Escudero <108844834+philipescudero@users.noreply.github.com>
2024-03-12 21:00:49 -0300

    Add files via upload

---
+++ app/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from.models import Categoria, Produto 
+
+# Register your models here.
+
+class ProdutoInline(admin.TabularInline):
+    model = Produto
+    extra = 1
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ["id", "nome"]
+    search_fields = ["nome"]
+    inlines = [ProdutoInline]
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ["id", "nome", "fabricante", "preco", "categoria"]
+    search_fields = ["nome"]
+
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Produto, ProdutoAdmin)
