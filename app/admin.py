985c0a69e6acdb8adb8fb43140c27958243d1c88
Philip Escudero <108844834+philipescudero@users.noreply.github.com>
2024-03-12 22:59:37 -0300

    Add files via upload

--- app/admin.py
+++ app/admin.py
@@ -1,20 +1,8 @@
 from django.contrib import admin
-from.models import Categoria, Produto 
+from.models import *
 
 # Register your models here.
 
-class ProdutoInline(admin.TabularInline):
-    model = Produto
-    extra = 1
-
-class CategoriaAdmin(admin.ModelAdmin):
-    list_display = ["id", "nome"]
-    search_fields = ["nome"]
-    inlines = [ProdutoInline]
-
-class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ["id", "nome", "fabricante", "preco", "categoria"]
-    search_fields = ["nome"]
-
-admin.site.register(Categoria, CategoriaAdmin)
-admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(ManterPessoas)
+admin.site.register(ManterCargos)
+admin.site.register()
--- app/models.py
+++ app/models.py
@@ -1,23 +1,70 @@
 from django.db import models
+from django.contrib import admin
+from app.models import *
 
-class Categoria(models.Model):
+
+class ManterPessoas(models.Model):
+    LOCADOR = 'LOCADOR'
+    LOCATARIO = 'LOCATARIO'
+    AVALISTA = 'AVALISTA'
+
+    TIPO_CHOICES = [
+        (LOCADOR, 'Locador'),
+        (LOCATARIO, 'Locatário'),
+        (AVALISTA, 'Avalista'),
+    ]
+
+    tipo = models.CharField(max_length=100, choices=TIPO_CHOICES)
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14)  
+    rg = models.CharField(max_length=20)  # Defina o comprimento do campo RG conforme necessário
+    data_nasc = models.DateField()
+    email = models.EmailField()
+    cidade = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nome} ({self.get_tipo_display()}) - {self.cpf} {self.rg} {self.data_nasc} {self.email} {self.cidade}"
+    
+class ManterCargos(models.Model):
+    GERENTE = 'GERENTE'
+    ASSISTENTE_ADM = 'ASSISTENTE ADM'
+
+    TIPO_CHOICES = [
+        (GERENTE, 'Gerente'),
+        (ASSISTENTE_ADM, 'Assistente ADM'),
+]
+
+    tipo = models.CharField(max_length=100, choices=TIPO_CHOICES)    
     nome = models.CharField(max_length=100)
 
-    class Meta:
-        verbose_name_plural = "Categorias"
+    def __str__(self):
+        return f"{self.nome} ({self.get_tipo_display()})" 
+
+class Manter_Tipo_Logradouro(models.Model):
+    AVENIDA = 'AVENIDA'
+    RUA = 'RUA'
+    ALAMEDA = 'ALAMEDA'
+    VIELA = 'VIELA'
+
+    TIPO_CHOICES = [
+        (AVENIDA, 'Avenida'),
+        (RUA, 'Rua'),
+        (ALAMEDA, 'Alameda'),
+        (VIELA, 'Viela')
+]
     
+    tipo = models.CharField(max_length=100, choices=TIPO_CHOICES)
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nome} ({self.get_tipo_display()})" 
+
+class Manter_Bairros(models.Model):
+    nome = models.CharField(max_length=100)
+
     def __str__(self):
         return self.nome
-
-class Produto(models.Model):
-    nome = models.CharField(max_length=100)
-    fabricante = models.CharField(max_length=100)
-    imagem = models.ImageField(upload_to='static/', blank=True)
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE)
-
-    class Meta:
-        verbose_name_plural = "Produtos"
     
-    def __str__(self):
-        return f'{self.categoria} {self.nome}'
+class Manter_Logradouros(models.Model):
+    
+
